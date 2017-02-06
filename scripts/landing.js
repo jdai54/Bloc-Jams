75a9abac56c6bab86648048ce6edd7cc7be4b3db
Jake <jdai54@gmail.com>
2017-02-05 22:57:02 -0800

    Assignment 9 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,19 +1,16 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-  
-  var revealPoint = function() {
-    for (var i = 0; i < points.length; i++) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-  }
-  
-  revealPoint();
-
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
+
+var animatePoints = function(points) {
+  forEach(points, revealPoint);
+};
+
 window.onload = function() {
   // Automatically animate the points on a tall screen where scrolling can't trigger the animation
   if (window.innerHeight > 950) {
