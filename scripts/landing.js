a2512b8d06310d88a58063fcc0cb02f915629363
Jake <jdai54@gmail.com>
2017-02-07 09:34:22 -0800

    Checkpoint 11 DOM completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,17 +2,16 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
   
-  var revealPoint = function() {
-    for (var i = 0; i < points.length; i++) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+  var revealPoint = function(index) {
+    points[i].style.opacity = 1;
+    points[i].style.transform = "scaleX(1) translateY(0)";
+    points[i].style.msTransform = "scaleX(1) translateY(0)";
+    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
   }
   
-  revealPoint();
-
+  for (var i = 0; i < points.length; i++) {
+    revealPoint(i);
+  }
 };
 window.onload = function() {
   // Automatically animate the points on a tall screen where scrolling can't trigger the animation
